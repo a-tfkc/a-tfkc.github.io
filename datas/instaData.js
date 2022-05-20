28832b5b6610b5fe5f6367ec455039a3565884e5
a-tfkc <aytentuefekci@web.de>
2022-05-21 00:04:53 +0200

    added photo & video pages + edited some sections

---
+++ datas/instaData.js
@@ -0,0 +1,34 @@
+const instaData = [
+    {
+      "webLink" : "https://www.instagram.com/p/CSpPmWvDb3W/?utm_source=ig_web_copy_link",
+      "imgSrc" : "/img/insta-1.jpg",
+      "imgAlt" : "Some Description"
+    },
+    {
+      "webLink" : "https://www.instagram.com/p/CSpPmWvDb3W/?utm_source=ig_web_copy_link",
+      "imgSrc" : "/img/insta-2.jpg",
+      "imgAlt" : "Some Description"
+    },
+    {
+      "webLink" : "https://www.instagram.com/p/CSpPmWvDb3W/?utm_source=ig_web_copy_link",
+      "imgSrc" : "/img/insta-3.jpg",
+      "imgAlt" : "Some Description"
+    },
+    {
+      "webLink" : "https://www.instagram.com/p/CSpPmWvDb3W/?utm_source=ig_web_copy_link",
+      "imgSrc" : "/img/insta-4.jpg",
+      "imgAlt" : "Some Description"
+    },
+    {
+      "webLink" : "https://www.instagram.com/p/CSpPmWvDb3W/?utm_source=ig_web_copy_link",
+      "imgSrc" : "/img/insta-1.jpg",
+      "imgAlt" : "Some Description"
+    },
+    {
+      "webLink" : "https://www.instagram.com/p/CSpPmWvDb3W/?utm_source=ig_web_copy_link",
+      "imgSrc" : "/img/insta-2.jpg",
+      "imgAlt" : "Some Description"
+    }
+]
+
+export default instaData
